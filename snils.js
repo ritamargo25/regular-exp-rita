ab9eeaf4f5d3718e7f10cfded8b578c6038e2724
Natalia <markina_n@resanta.ru>
2023-02-24 21:30:13 +0300

    regulyarki mat' ih

---
+++ snils.js
@@ -0,0 +1,43 @@
+// Изучи тесты `spec/snils.spec.js`
+// Чтобы понимать как должны работать твои методы.
+
+
+// Определи, есть ли во входной строке номер СНИЛС.
+function hasSNILS(string) {
+
+}
+
+
+// Найди и верни номер СНИЛС из строки.
+function grabSNILS(string) {
+
+}
+
+
+// Найди и верни все номера СНИЛС, что есть в строке.
+function grabAllSNILS(string) {
+
+}
+
+//console.log(grabAllSNILS("The numbers are 350-802-074 94, 234-600-142 22, and 013-605-876 94"))
+
+// Зашифруй номера СНИЛС. Example: XXX-XXX-XXX 30.
+function hideAllSNILS(string) {
+
+}
+
+
+
+// Отформатируй все номера СНИЛС, чтобы использовались правильные разделители:
+// '48001443027', '480.014.430.27', и '480--014--430 27' должны превратиться в '480-014-430 27'.
+function formatSNILS(string) {
+
+}
+
+module.exports = {
+hasSNILS,
+grabSNILS,
+grabAllSNILS,
+hideAllSNILS,
+formatSNILS
+};
